b83335c1a9ecb1f519f27d400d65099138b5e8d1
kathy <kathy_sunshine@163.com>
2019-08-11 15:25:53 +0800

    fourth commit

--- page/setting_page.py
+++ page/setting_page.py
@@ -6,6 +6,11 @@ from base.base_action import BasePage
 
 class SettingPage(BasePage):
     about_aolai_btn = By.XPATH,"//*[@text='关于百年奥莱']"
+    clear_cache_btn = By.ID,"com.yunmall.lc:id/setting_clear_cache"
     @allure.step(title='点击百年奥莱')
     def click_about_aolai_btn(self):
         self.find_element_with_scroll(self.about_aolai_btn).click()
+
+    @allure.step(title='点击清除缓存')
+    def click_clear_cache_btn(self):
+        self.find_element_with_scroll(self.clear_cache_btn).click()
---
+++ scripts/test_clear_cache.py
@@ -0,0 +1,14 @@
+from base.base_driver import init_driver
+from page.page import Page
+
+
+class TestClearCache:
+    def setup(self):
+        self.driver = init_driver(True)
+        self.page = Page()
+    def test_clear_cache(self):
+        self.page.home.login_is_not(self.page)
+        self.page.me.click_setting_btn()
+        self.page.setting.click_clear_cache_btn()
+
+        assert self.page.about.is_toast_exist("清理成功")
